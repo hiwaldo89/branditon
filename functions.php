dc53032de91edd1bd7be34bf4089a14fb342b489
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-19 14:55:01 -0600

    add structure files

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!function_exists('branditon_setup')) {
+    function branditon_setup() {
+        // Make theme available for translation.
+        load_theme_textdomain( 'branditon', get_template_directory() . '/languages' );
+
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support( 'automatic-feed-links' );
+        
+        // Let WordPress manage the document title.
+        add_theme_support( 'title-tag' );
+
+        // Enable support for Post Thumbnails on posts and pages.
+        add_theme_support( 'post-thumbnails' );
+
+        add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+    }
+}
+add_action( 'after_setup_theme', 'branditon_setup' );
+
+// Enqueue scripts and styles.
+function branditon_scripts() {
+	wp_enqueue_style( 'branditon-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'branditon_scripts' );
