bab2252455d84b5ac4ab30c9bc1099a7c848b39d
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-21 16:40:46 -0600

    add footer

---
+++ components/footer.php
@@ -0,0 +1,12 @@
+<footer class="branditon-footer">
+    <div class="flex flex-wrap">
+        <div class="w-full md:w-1/2 branditon-footer__social text-center py-24">
+            <h2>
+                Creating a brand isn't 
+                <br>
+                about creating a logo
+            </h2>
+        </div>
+        <div class="w-full md:w-1/2 branditon-footer__newsletter"></div>
+    </div>
+</footer>
--- components/homepage/instagram-feed.php
+++ components/homepage/instagram-feed.php
@@ -3,4 +3,7 @@
         <h2 class="text-2xl">INSTAGRAM</h2>
         <a href="" class="text-2xl">@brand.it.on</a>
     </div>
+    <div class="instagram-feed__slider">
+        Here goes the instagram feed
+    </div>
 </div>
