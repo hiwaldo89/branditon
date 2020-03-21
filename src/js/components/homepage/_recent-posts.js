fa750b2977e2eb7a4b565c2056c650c74689049d
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-21 15:51:08 -0600

    adding pagination to recent posts

--- src/js/components/homepage/_recent-posts.js
+++ src/js/components/homepage/_recent-posts.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 if (document.querySelector('.homepage-recent-posts__wrapper')) {
   $('.homepage-recent-posts__wrapper').slick({
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     arrows: false
   });
 
