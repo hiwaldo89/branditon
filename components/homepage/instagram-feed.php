74207a634080bd2d7d14d97afb00f44d232c4c7c
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-21 16:00:15 -0600

    adding pagination to recent posts

--- components/homepage/instagram-feed.php
+++ components/homepage/instagram-feed.php
@@ -1,6 +1,6 @@
-<div class="instagram-feed">
+<div class="instagram-feed py-12">
     <div class="container mx-auto text-center">
-        <h2>INSTAGRAM</h2>
-        <a href="">@brand.it.on</a>
+        <h2 class="text-lg">INSTAGRAM</h2>
+        <a href="" class="text-lg">@brand.it.on</a>
     </div>
 </div>
