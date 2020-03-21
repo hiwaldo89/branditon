1f8f1fe6f93ff6d6795733ac7dd790efc8a2e8c0
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-20 20:24:06 -0600

    test image in recent posts

--- components/homepage/recent-posts.php
+++ components/homepage/recent-posts.php
@@ -16,5 +16,16 @@
                 a new post
             </div>
         <?php endwhile; endif; wp_reset_postdata(); ?>
+        <?php if($recentPostsQuery->have_posts()) : ?>
+            <div class="flex flex-wrap">
+                <?php while($recentPostsQuery->have_posts()) : $recentPostsQuery->the_post(); ?>
+                    <div class="homepage-recent-posts__article w-full md:w-4/12">
+                        <div class="homepage-recent-posts__article-img">
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        <?php endif; wp_reset_postdata(); ?>
     </div>
 </div>
