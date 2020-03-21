be51ac1527f5638eda888218a8fa17fdbd771b17
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-21 15:59:17 -0600

    adding pagination to recent posts

---
+++ components/homepage/instagram-feed.php
@@ -0,0 +1,6 @@
+<div class="instagram-feed">
+    <div class="container mx-auto text-center">
+        <h2>INSTAGRAM</h2>
+        <a href="">@brand.it.on</a>
+    </div>
+</div>
--- page-home.php
+++ page-home.php
@@ -5,5 +5,6 @@ get_header();
 <main>
     <?php get_template_part('components/homepage/about'); ?>
     <?php get_template_part('components/homepage/recent-posts'); ?>
+    <?php get_template_part('components/homepage/instagram-feed'); ?>
 </main>
 <?php get_footer(); ?>
