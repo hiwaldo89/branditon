5e7c8ec7c8dd53b7df2cb3999befd10615bf36b9
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-20 20:46:26 -0600

    test displayin date in recent posts

--- components/homepage/recent-posts.php
+++ components/homepage/recent-posts.php
@@ -19,7 +19,7 @@
                             <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
                         </div>
                         <div class="text-center">
-                            <?php $f = new NumberFormatter("en", NumberFormatter::SPELLOUT); ?>
+                            <?php $f = new NumberFormatter("es", NumberFormatter::SPELLOUT); ?>
                             <?php echo the_time('j'); ?>
                             <?php echo the_time('F'); ?> <?php echo $f->format(the_time('j')); ?>, <br>
                             <?php echo $f->format(get_the_time('Y')); ?>
