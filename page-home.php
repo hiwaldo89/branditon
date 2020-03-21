c407089ba37e8c32a31b6788c4bb8961b5c05cc7
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-20 20:12:17 -0600

    add recent posts component to homepage

--- page-home.php
+++ page-home.php
@@ -4,5 +4,6 @@ get_header();
 ?>
 <main>
     <?php get_template_part('components/homepage/about'); ?>
+    <?php get_template_part('components/homepage/recent-posts'); ?>
 </main>
 <?php get_footer(); ?>
