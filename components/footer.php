e21e8eced9bb65d63831876324e5431af0d9132b
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-21 16:48:51 -0600

    add social icons to footer

--- components/footer.php
+++ components/footer.php
@@ -8,7 +8,7 @@
             </h2>
             <div class="flex items-center justify-center">
                 <a href="https://www.instagram.com/brand.it.on/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/instagram-circle.svg" alt="brand.it.on"></a>
-                <a href="https://www.facebook.com/branditon.mx/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/facebook-circle.svg" alt="branditon.mx"></a>
+                <a href="https://www.facebook.com/branditon.mx/" target="_blank" class="mx-4"><img src="<?php bloginfo('template_url'); ?>/img/facebook-circle.svg" alt="branditon.mx"></a>
                 <a href="mailto:fernanda@branditon.com.mx"><img src="<?php bloginfo('template_url'); ?>/img/email-circle.svg" alt="fernanda@branditon.com.mx"></a>
             </div>
         </div>
