407b2227d34049240a3db14417f924c486a6d475
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-21 15:50:23 -0600

    adding pagination to recent posts

--- src/js/components/homepage/_recent-posts.js
+++ src/js/components/homepage/_recent-posts.js
@@ -14,7 +14,7 @@ if (document.querySelector('.homepage-recent-posts__wrapper')) {
   recentPostsDots.forEach(dot => {
     dot.onclick = e => {
       e.preventDefault();
-      console.log(dot.dataset.slide);
+      console.log(Number(dot.dataset.slide));
       console.log(
         $('.homepage-recent-posts__wrapper').slick('slickCurrentSlide')
       );
