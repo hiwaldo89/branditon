e21531ea3f9774a83fa37c5593106b05371ab8fd
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-20 19:12:27 -0600

    remove esc_url from echo in about component

--- components/homepage/about.php
+++ components/homepage/about.php
@@ -3,13 +3,13 @@
     <div class="homepage-about">
         <div class="container mx-auto flex">
             <div class="w-full md:w-4/12">
-                <h2><?php echo esc_url($aboutSection['headline']); ?></h2>
+                <h2><?php echo $aboutSection['headline']; ?></h2>
                 <div class="text-justify">
-                    <?php echo esc_url($aboutSection['description']); ?>
+                    <?php echo $aboutSection['description']; ?>
                 </div>
             </div>
             <div class="w-full md:w-8/12">
-                
+
             </div>
         </div>
     </div>
