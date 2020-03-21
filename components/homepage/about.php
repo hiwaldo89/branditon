a1c2782ff129c33a654a60937ae791e262bf59d6
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-20 19:15:14 -0600

    change font style in about component

--- components/homepage/about.php
+++ components/homepage/about.php
@@ -1,10 +1,10 @@
 <?php $aboutSection = get_field('about_section'); ?>
 <?php if ($aboutSection) : ?>
-    <div class="homepage-about">
+    <div class="homepage-about py-12">
         <div class="container mx-auto flex">
             <div class="w-full md:w-4/12">
-                <h2><?php echo $aboutSection['headline']; ?></h2>
-                <div class="text-justify">
+                <h2 class="text-4xl"><?php echo $aboutSection['headline']; ?></h2>
+                <div class="text-justify text-xl">
                     <?php echo $aboutSection['description']; ?>
                 </div>
             </div>
