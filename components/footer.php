fd6672f91f9ab3e37ba5b8cfa75387240dad518e
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-21 16:46:24 -0600

    add icons to footer

--- components/footer.php
+++ components/footer.php
@@ -1,11 +1,16 @@
-<footer class="branditon-footer">
+<footer class="branditon-footer mt-12">
     <div class="flex flex-wrap">
         <div class="w-full md:w-1/2 branditon-footer__social text-center py-24">
-            <h2>
+            <h2 class="text-4xl">
                 Creating a brand isn't 
                 <br>
                 about creating a logo
             </h2>
+            <div class="flex items-center">
+                <a href="https://www.instagram.com/brand.it.on/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/instagram-circle.svg" alt="brand.it.on"></a>
+                <a href="https://www.facebook.com/branditon.mx/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/facebook-circle.svg" alt="branditon.mx"></a>
+                <a href="mailto:fernanda@branditon.com.mx"><img src="<?php bloginfo('template_url'); ?>/img/email-circle.svg" alt="fernanda@branditon.com.mx"></a>
+            </div>
         </div>
         <div class="w-full md:w-1/2 branditon-footer__newsletter"></div>
     </div>
