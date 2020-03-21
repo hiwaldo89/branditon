86618da4e81a6196a4c5f752b1d324e1fec62405
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-20 18:58:23 -0600

    move header to component

--- header.php
+++ header.php
@@ -7,42 +7,4 @@
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<header class="branditon-header py-4">
-    <div class="container mx-auto flex items-center">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="mr-auto branditon-header__logo w-1/2 md:w-3/12">
-            <img src="<?php bloginfo('template_url'); ?>/img/branditon-logo.svg" alt="branditon">
-        </a>
-        <nav class="mr-3 w-full md:w-1/2 flex items-center">
-            <ul class="flex w-full justify-between">
-                <li class="relative">
-                    <span class="absolute inset-0">ABOUT</span>
-                    <a href="" class="text-4xl">
-                        about
-                    </a>
-                </li>
-                <li>
-                    <span class="absolute inset-0">BLOG</span>
-                    <a href="" class="text-4xl">
-                        blog
-                    </a>
-                </li>
-                <li>
-                    <span class="absolute inset-0">PRESS</span>
-                    <a href="" class="text-4xl">
-                        press
-                    </a>
-                </li>
-                <li>
-                    <span class="absolute inset-0">CONTACTO</span>
-                    <a href="" class="text-4xl">
-                        contacto
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <div class="branditon-header__social flex md:w-3/12 justify-end items-center">
-            <a href="https://www.instagram.com/brand.it.on/" target="_blank" class="mr-4"><img src="<?php bloginfo('template_url'); ?>/img/instagram.svg" alt="brand.it.on"></a>
-            <a href="https://www.facebook.com/branditon.mx/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/facebook.svg" alt="branditon.mx"></a>
-        </div>
-    </div>
-</header>
+<?php get_template_part('components/header'); ?>
