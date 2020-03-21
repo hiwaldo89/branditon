700d1dc462575a2730258f96ee0ab04f0c27bead
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-20 19:54:26 -0600

    add decoration to about component

--- components/homepage/about.php
+++ components/homepage/about.php
@@ -2,12 +2,13 @@
 <?php if ($aboutSection) : ?>
     <div class="homepage-about py-12">
         <div class="container mx-auto flex">
-            <div class="w-full md:w-4/12">
+            <div class="w-full md:w-4/12 lg:w-3/12">
                 <span class="homepage-about__line-deco"></span>
                 <h2 class="text-4xl text-center tracking-widest"><?php echo $aboutSection['headline']; ?></h2>
                 <div class="text-justify text-xl">
                     <?php echo $aboutSection['description']; ?>
                 </div>
+                <span class="homepage-about__cross-deco"></span>
             </div>
             <div class="w-full md:w-7/12 md:ml-auto homepage-about__images relative">
                 <div class="w-full md:w-7/12 homepage-about__front-img">
