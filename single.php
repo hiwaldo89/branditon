aac6fce8e0280a14ef4fa25105d6d24e18d9a468
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 13:46:36 -0600

    building single post structure

--- single.php
+++ single.php
@@ -1,3 +1,5 @@
 <?php get_header(); ?>
-    <?php get_template_part('components/blog/branditon-article'); ?>
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+        <?php get_template_part('components/blog/branditon-article'); ?>
+    <?php endwhile; endif; ?>
 <?php get_footer(); ?>
