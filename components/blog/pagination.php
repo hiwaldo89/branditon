93084a08649031970fe8c00026bf2140985bbf40
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 16:44:52 -0600

    add featured press component

--- components/blog/pagination.php
+++ components/blog/pagination.php
@@ -1,4 +1,3 @@
 <div class="blog-pagination mt-12">
-    <?php var_dump($customQuery); ?>
-    <?php branditon_numeric_posts_nav($custom_query); ?>
+    <?php branditon_numeric_posts_nav($customQuery); ?>
 </div>
