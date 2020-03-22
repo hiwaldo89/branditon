b9203226690399b48976bad171d0c743582ea4fb
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 09:55:41 -0600

    add blog headline

---
+++ home.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+    <main>
+        <div class="container mx-auto text-center">
+            <h2 class="text-3xl pt-20 mb-12"><?php the_field('headline'); ?></h2>
+        </div>
+    </main>
+<?php get_footer(); ?>
