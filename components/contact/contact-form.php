281ef719fe14d82a4be7efd3ac7746b07dfa11e2
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 17:49:53 -0600

    build contact page

---
+++ components/contact/contact-form.php
@@ -0,0 +1,17 @@
+<div class="contact-form">
+    <div class="container mx-auto">
+        <div class="flex flex-wrap">
+            <div class="w-full lg:w-8/12 mx-auto">
+                <form action="">
+                    <input type="text" placeholder="Nombre">
+                    <input type="tel" placeholder="Teléfono">
+                    <input type="email" placeholder="Correo">
+                    <textarea placeholder="Mensaje"></textarea>
+                    <div class="text-center">
+                        <button type="button">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- page-contact.php
+++ page-contact.php
@@ -4,5 +4,6 @@ get_header();
 ?>
     <main>
         <?php get_template_part('components/contact/contact-section'); ?>
+        <?php get_template_part('components/contact/contact-form'); ?>
     </main>
 <?php get_footer(); ?>
