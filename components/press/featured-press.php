f91090d3e18605fd364cbcbc7cce54db43c6a4c0
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 14:56:23 -0600

    add featured press component

--- components/press/featured-press.php
+++ components/press/featured-press.php
@@ -2,4 +2,12 @@
     <div class="featured-press__img w-full lg:w-8/12">
         <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
     </div>
+    <div class="w-full lg:w-4/12 featured-press__content text-center">
+        <?php 
+            $mediaPublication = get_taxonomies(array(
+                'name' => array('publication_media')
+            )); 
+            var_dump($mediaPublication);
+        ?>
+    </div>
 </div>
