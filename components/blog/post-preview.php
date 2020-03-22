0970f090ae7dae4c34339faeae1e042c2bf0f022
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 10:13:04 -0600

    add image in post preview

--- components/blog/post-preview.php
+++ components/blog/post-preview.php
@@ -1,6 +1,6 @@
 <div class="post-preview">
     <a href="<?php the_permalink(); ?>" class="flex flex-wrap">
-        <div class="post-preview__img post-preview__col relative">
+        <div class="post-preview__img post-preview__col relative w-full lg:w-1/2">
             <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="absolute w-full h-full top-0 left-0 object-fit">
         </div>
     </a>
