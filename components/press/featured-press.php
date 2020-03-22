5cf311989f5115936cbf1cf336d7f2cb4514c51e
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 14:52:02 -0600

    add press template

---
+++ components/press/featured-press.php
@@ -0,0 +1,5 @@
+<div class="featured-press flex flex-wrap">
+    <div class="featured-press__img w-full lg:w-8/12">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+    </div>
+</div>
--- page-press.php
+++ page-press.php
@@ -3,6 +3,18 @@
 get_header();
 ?>
     <main>
-        
+        <div class="container mx-auto">
+            <h2 class="text-center text-xl py-24">Nota más reciente</h2>
+            <?php 
+                $recentPostArgs = array(
+                    'post_type' => 'press',
+                    'posts_per_page' => 1
+                );
+                $recentPost = new WP_Query($recentPostArgs);
+            ?>
+            <?php if($recentPost->have_posts()) : while($recentPost->have_posts()) : $recentPost->the_post(); ?>
+                <?php get_template_part('components/press/featured-press'); ?>
+            <?php endwhile; endif; ?>
+        </div>
     </main>
 <?php get_footer(); ?>
