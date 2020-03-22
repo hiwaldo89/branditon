9a39edc07f300ac90cbca8af671ccaa458969ba9
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 10:36:29 -0600

    building post preview

--- components/blog/post-preview.php
+++ components/blog/post-preview.php
@@ -7,12 +7,14 @@
             <div class="post-preview__col w-full lg:w-1/2">
                 <div class="flex flex-wrap">
                     <div class="post-preview__desc w-full lg:w-8/12 pt-8">
-                        <div class="post-preview__cat text-center text-lg mb-8">BRANDING</div>
+                        <?php $categories = get_the_category(); ?>
+                        <div class="post-preview__cat text-center text-lg mb-8"><?php echo $categories[0]; ?></div>
                         <h3 class="text-center text-2xl"><?php the_title(); ?></h3>
                         <div class="text-left">
                             <?php the_excerpt(); ?>
                         </div>
                     </div>
+                    <div class="post-preview__meta"></div>
                 </div>
             </div>
         </a>
