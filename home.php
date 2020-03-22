9eda4f41f07c05ee0081823d22c4c81dec821196
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 10:09:49 -0600

    add image in post preview

--- home.php
+++ home.php
@@ -3,6 +3,9 @@
         <?php if(have_posts()) : ?>
             <div class="container mx-auto text-center">
                 <h2 class="text-3xl pt-20 mb-12"><?php the_field('headline', get_option('page_for_posts')); ?></h2>
+                <?php while(have_posts()) : the_post(); ?>
+                    <?php get_template_part('components/blog/post-preview'); ?>
+                <?php endwhile; wp_reset_postdata; ?>
             </div>
         <?php endif; ?>
     </main>
