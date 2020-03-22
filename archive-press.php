c2f214fcfe0b860c85de6e2b7ed63101078b2d74
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 17:22:40 -0600

    test press pagination

--- archive-press.php
+++ archive-press.php
@@ -29,20 +29,7 @@
                         <?php get_template_part('components/press/press-preview'); ?>
                     </div>
                     <?php endwhile; ?>
-                    <?php
-					$total_pages = $pressQuery->max_num_pages;
-					if ($total_pages > 1) {
-						$current_page = max(1, get_query_var( 'paged' ));
-						echo paginate_links( array(
-							'base' => get_pagenum_link( 1 ) . '%_%',
-							'format' => 'page/%#%',
-							'current' => $current_page,
-							'total' => $total_pages,
-							'prev_text' => __('PREVIOUS'),
-							'next_text' => __('NEXT'),
-						) );
-					}
-					?>
+                    <?php get_template_part('content/blog/pagination'); ?>
                 <?php endif; wp_reset_postdata(); ?>
             </div>
         </div>
