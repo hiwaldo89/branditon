2c240854db781997bfe61b8201cd8b766e24add5
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 14:09:17 -0600

    add share button

--- components/blog/share-article.php
+++ components/blog/share-article.php
@@ -1,8 +1,8 @@
-<div class="share-article">
+<div class="share-article py-24">
     <div class="container mx-auto text-center">
         <a href="" class="uppercase flex items-center justify-center">
             COMPARTE ESTE ARTÍCULO
-            <span class="share-article__plus"></span>
+            <span class="share-article__plus ml-4"></span>
         </a>
     </div>
 </div>
