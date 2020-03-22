c7f80dc07956805e93b345d1cb195421658c034a
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 14:31:06 -0600

    add post navigation

--- components/blog/post-nav.php
+++ components/blog/post-nav.php
@@ -1,5 +1,8 @@
 <div class="post-nav flex flex-wrap">
     <?php $prevPost = get_previous_post(); ?>
+    <?php if(!isset($prevPost) || strlen($prevPost->post_title) <= 0) : ?>
+        <?php $prevPost = get_posts()[0]; ?>
+    <?php endif; ?>
     <a href="<?php echo get_permalink($prevPost->ID); ?>" class="w-full lg:w-1/2 post-nav__prev text-right pt-24 px-20 pb-20">
         <span class="text-lg">ANTERIOR</span>
         <h3 class="text-2xl"><?php echo $prevPost->post_title; ?></h3>
