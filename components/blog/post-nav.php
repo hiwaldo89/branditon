0210314b642c939918f826f0b98d2347924a2018
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 14:29:53 -0600

    add post navigation

--- components/blog/post-nav.php
+++ components/blog/post-nav.php
@@ -5,6 +5,9 @@
         <h3 class="text-2xl"><?php echo $prevPost->post_title; ?></h3>
     </a>
     <?php $nextPost = get_next_post(); ?>
+    <?php if(!isset($nextPost) || strlen($nextPost->post_title) <= 0) : ?>
+        <?php $nextPost = get_posts( 'numberposts=1&order=ASC' )[0]; ?>
+    <?php endif; ?>
     <a href="<?php echo get_permalink($nextPost->ID); ?>" class="w-full lg:w-1/2 post-nav__next pt-24 px-20">
         <span class="text-lg">SIGUIENTE</span>
         <h3 class="text-2xl"><?php echo $nextPost->post_title; ?></h3>
