d657361eb89bf6c5a051b6e43b03452057171ba9
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 17:21:26 -0600

    test press pagination

--- archive-press.php
+++ archive-press.php
@@ -1,3 +1,37 @@
 <?php get_header(); ?>
-hello there
+    <main>
+        <div class="container mx-auto">
+            <h2 class="text-center text-xl py-24">Nota más reciente</h2>
+            <?php 
+                $recentPostArgs = array(
+                    'post_type' => 'press',
+                    'posts_per_page' => 1
+                );
+                $recentPost = new WP_Query($recentPostArgs);
+            ?>
+            <?php if($recentPost->have_posts()) : while($recentPost->have_posts()) : $recentPost->the_post(); ?>
+                <?php get_template_part('components/press/featured-press'); ?>
+            <?php endwhile; endif; wp_reset_postdata(); ?>
+            <h2 class="text-center text-xl py-24">Encuentra más notas</h2>
+            <?php 
+                $pressArgs = array(
+                    'post_type' => 'press',
+                    //'offset' => 1,
+                    'posts_per_page' => 2,
+                    'paged' => (get_query_var( 'paged' )) ? get_query_var( 'paged' ) : 1
+                );
+                $pressQuery = new WP_Query($pressArgs);
+            ?>
+            <div class="flex flex-wrap -mx-4">
+                <?php if(have_posts()) : ?>
+                    <?php while(have_posts()) : the_post(); ?>
+                    <div class="w-full lg:w-4/12 px-4">
+                        <?php get_template_part('components/press/press-preview'); ?>
+                    </div>
+                    <?php endwhile; ?>
+                    
+                <?php endif; wp_reset_postdata(); ?>
+            </div>
+        </div>
+    </main>
 <?php get_footer(); ?>
