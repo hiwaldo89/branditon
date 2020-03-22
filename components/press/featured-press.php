b3ac53aad17b4c9c1ab16f4399c8de7c63d97f91
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 15:22:24 -0600

    add featured press component

--- components/press/featured-press.php
+++ components/press/featured-press.php
@@ -8,12 +8,12 @@
             <?php 
                 $mediaPublication = get_the_terms(get_the_ID(), 'publication_media'); 
             ?>
-            <span class="featured-press__cat uppercase text-xl"><?php echo $mediaPublication[0]->name; ?></span>
-            <h2 class="text-2xl"><?php the_title(); ?></h2>
-            <span class="featured-press__date">
+            <span class="featured-press__cat uppercase text-lg inline-block mb-6"><?php echo $mediaPublication[0]->name; ?></span>
+            <h2 class="text-3xl"><?php the_title(); ?></h2>
+            <span class="featured-press__date inline-block mb-8">
                 <?php echo ucfirst(get_the_time('F')); ?> <?php echo $f->format(get_the_time('j')); ?>, <?php echo $f->format(get_the_time('Y')); ?>.
             </span>
-            <p><?php the_excerpt(); ?></p>
+            <?php the_content(); ?>
         </div>
     </a>
 </div>
