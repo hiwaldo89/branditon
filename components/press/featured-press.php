b55fb65d5a925f84ee1e15a63aece13e12b2dc65
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 15:16:34 -0600

    add featured press component

--- components/press/featured-press.php
+++ components/press/featured-press.php
@@ -1,14 +1,15 @@
 <?php $f = new NumberFormatter("es", NumberFormatter::SPELLOUT); ?>
-<div class="featured-press flex flex-wrap">
-    <a href="<?php the_field('article_link'); ?>" target="_blank">
+<div class="featured-press">
+    <a href="<?php the_field('article_link'); ?>" target="_blank" class="flex flex-wrap">
         <div class="featured-press__img w-full lg:w-8/12 relative">
             <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="absolute w-full h-full object-cover top-0 left-0">
         </div>
         <div class="w-full lg:w-4/12 featured-press__content text-center">
             <?php 
-                $mediaPublication = get_terms('publication_media'); 
+                $mediaPublication = get_the_terms(get_the_ID(), 'publication_media'); 
                 var_dump($mediaPublication);
             ?>
+            <span class="featured-press__cat"><?php echo $mediaPublication[0]->name; ?></span>
             <h2 class="text-2xl"><?php the_title(); ?></h2>
             <span class="featured-press__date">
                 <?php echo ucfirst(get_the_time('F')); ?> <?php echo $f->format(get_the_time('j')); ?>, <?php echo $f->format(get_the_time('Y')); ?>.
