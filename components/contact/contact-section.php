7116b02669e702a8f91c6f0c0a16289d0983c36d
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 17:56:19 -0600

    build contact page

--- components/contact/contact-section.php
+++ components/contact/contact-section.php
@@ -4,7 +4,7 @@
     <a href="mailto:fernanda@branditon.com.mx" class="inline-block text-3xl mb-8 contact-section__email">fernanda@branditon.com.mx</a>
     <div class="flex items-center justify-center contact-section__social mb-8">
         <a href="https://www.instagram.com/brand.it.on/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/instagram-circle.svg" alt="instagram"></a>
-        <a href="https://www.facebook.com/branditon.mx/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/facebook-circle.svg" alt="facebook"></a>
+        <a href="https://www.facebook.com/branditon.mx/" target="_blank" class="mx-4"><img src="<?php bloginfo('template_url'); ?>/img/facebook-circle.svg" alt="facebook"></a>
         <a href="mailto:fernanda@branditon.com.mx"><img src="<?php bloginfo('template_url'); ?>/img/email-circle.svg" alt="email"></a>
     </div>
     <p class="text-3xl">
