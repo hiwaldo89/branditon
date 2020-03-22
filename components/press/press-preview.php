5aeabecef428054c3a9eb3bcc69e935aac24a958
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 15:30:20 -0600

    add featured press component

--- components/press/press-preview.php
+++ components/press/press-preview.php
@@ -1,7 +1,16 @@
-<div class="press-preview">
+<?php $f = new NumberFormatter("es", NumberFormatter::SPELLOUT); ?>
+<div class="press-preview mb-8">
     <a href="<?php the_permalink(); ?>">
         <div class="press-preview__img relative">
             <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="absolute w-full h-full top-0 left-0 object-cover">
+        </div>
+        <div class="press-preview__content text-center px-4">
+            <?php $mediaPublication = get_the_terms(get_the_ID(), 'publication_media'); ?>
+            <span class="inline-block press-preview__cat uppercase text-lg"><?php echo $mediaPublication[0]->name; ?></span>
+            <h2 class="text-2xl"><?php the_title(); ?></h2>
+            <span class="press-preview__date">
+                <?php echo ucfirst(get_the_time('F')); ?> <?php echo $f->format(get_the_time('j')); ?>, <?php echo $f->format(get_the_time('Y')); ?>.
+            </span>
         </div>
     </a>
 </div>
