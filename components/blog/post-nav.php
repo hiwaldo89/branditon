9c7aa0a90a7d24c50b20d60ef029360f32aca248
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 14:19:28 -0600

    add post navigation

--- components/blog/post-nav.php
+++ components/blog/post-nav.php
@@ -2,11 +2,11 @@
     <div class="w-full lg:w-1/2 post-nav__prev text-right">
         <?php $prevPost = get_previous_post(); ?>
         <span>ANTERIOR</span>
-        <h3><?php $prevPost->post_title; ?></h3>
+        <h3><?php echo $prevPost->post_title; ?></h3>
     </div>
     <div class="w-full lg:w-1/2 post-nav__next">
         <?php $nextPost = get_next_post(); ?>
         <span>SIGUIENTE</span>
-        <h3><?php $nextPost->post_title; ?></h3>
+        <h3><?php echo $nextPost->post_title; ?></h3>
     </div>
 </div>
--- single.php
+++ single.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?>
     <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <?php get_template_part('components/blog/branditon-article'); ?>
-    <?php endwhile; endif; wp_reset_postdata(); ?>
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <?php get_template_part('components/blog/share-article'); ?>
         <?php get_template_part('components/blog/post-nav'); ?>
     <?php endwhile; endif; wp_reset_postdata(); ?>
