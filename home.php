64d2fd353c6007b7c6c5c7376552aca3a3ac98d0
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 09:57:44 -0600

    add blog headline

--- home.php
+++ home.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
     <main>
-        <div class="container mx-auto text-center">
-            <h2 class="text-3xl pt-20 mb-12"><?php the_field('headline'); ?></h2>
-        </div>
+        <?php if(have_posts()) : ?>
+            <div class="container mx-auto text-center">
+                <h2 class="text-3xl pt-20 mb-12"><?php the_field('headline'); ?></h2>
+            </div>
+        <?php endif; ?>
     </main>
 <?php get_footer(); ?>
