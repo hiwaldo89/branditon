22baeaa5a32fabd21a349ad8cc8e0f9311d1f8b0
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 09:44:13 -0600

    add about page structure

--- components/about/about-branditon.php
+++ components/about/about-branditon.php
@@ -1,6 +1,6 @@
 <div class="container mx-auto about-branditon py-12">
-    <h2 class="text-center text-3xl mb-12">Acerca de Brand it on</h2>
-    <div class="flex about-branditon__content mb-8">
+    <h2 class="text-center text-3xl mb-16">Acerca de Brand it on</h2>
+    <div class="flex about-branditon__content mb-12">
         <div class="w-full lg:w-1/2 text-justify about-branditon__col text-xl">
             <?php the_field('left_column'); ?>
         </div>
