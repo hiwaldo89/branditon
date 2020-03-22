8057ca723050ff9ddffab6024557367e58c5d914
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 17:40:39 -0600

    build contact page

---
+++ components/contact/contact-section.php
@@ -0,0 +1,10 @@
+<div class="container py-12 mx-auto text-center contact-section">
+    <h2 class="text-3xl">¿Cómo puedes comunicarte directamente conmigo?</h2>
+    <p class="text-3xl">Envíame un correo a:</p>
+    <a href="mailto:fernanda@branditon.com.mx" class="inline-block text-3xl" class="contact-section__email">fernanda@branditon.com.mx</a>
+    <div class="flex items-center justify-center contact-section__social">
+        <a href="https://www.instagram.com/brand.it.on/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/instagram-circle.svg" alt="instagram"></a>
+        <a href="https://www.facebook.com/branditon.mx/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/facebook-circle.svg" alt="facebook"></a>
+        <a href="mailto:fernanda@branditon.com.mx"><img src="<?php bloginfo('template_url'); ?>/img/email-circle.svg" alt="email"></a>
+    </div>
+</div>
---
+++ page-contact.php
@@ -0,0 +1,8 @@
+<?php 
+/* Template Name: Contact */
+get_header();
+?>
+    <main>
+        <?php get_template_part('components/contact/contact-section'); ?>
+    </main>
+<?php get_footer(); ?>
