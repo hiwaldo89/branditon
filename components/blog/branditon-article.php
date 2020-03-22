f6a02d1bb51be945cfbb0b947c5146df1987a116
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 12:58:33 -0600

    add custom pagination

---
+++ components/blog/branditon-article.php
@@ -0,0 +1,13 @@
+<article class="branditon-article">
+    <div class="flex flex-wrap branditon-article__prev">
+        <div class="lg:w-3/12 text-right ">
+            <a href="">
+                ANTERIOR
+            </a>
+        </div>
+        <div class="lg:2-6/12">
+            <h1 class="text-center mt-6 text-3xl px-12"><?php the_title(); ?></h1>
+        </div>
+        <div class="lg:w-3/12"></div>
+    </div>
+</article>
---
+++ single.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+    <?php get_template_part('components/blog/branditon-article'); ?>
+<?php get_footer(); ?>
