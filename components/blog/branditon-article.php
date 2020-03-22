72fea2984aab8d3b1599c1692e6da14a65b48915
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 13:10:52 -0600

    add custom pagination

--- components/blog/branditon-article.php
+++ components/blog/branditon-article.php
@@ -1,6 +1,6 @@
 <article class="branditon-article">
     <div class="flex flex-wrap">
-        <div class="lg:w-3/12 text-right branditon-article__prev">
+        <div class="lg:w-2/12 text-right branditon-article__prev">
             <?php $prevPost = get_previous_post(); ?>
             <?php if(isset($prevPost)) : ?>
                 <a href="<?php echo get_permalink($prevPost->ID) ?>" class="block relative text-lg">
@@ -12,6 +12,6 @@
         <div class="lg:w-4/12 mx-auto">
             <h1 class="text-center mt-6 text-3xl px-12"><?php the_title(); ?></h1>
         </div>
-        <div class="lg:w-3/12"></div>
+        <div class="lg:w-2/12"></div>
     </div>
 </article>
