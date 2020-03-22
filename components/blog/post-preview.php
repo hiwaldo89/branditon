db203d95eccecbd07c5eccc13fbc84b6c07c0504
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 10:28:30 -0600

    building post preview

--- components/blog/post-preview.php
+++ components/blog/post-preview.php
@@ -1,16 +1,20 @@
 <div class="post-preview mb-12">
-    <a href="<?php the_permalink(); ?>" class="flex flex-wrap">
-        <div class="post-preview__img post-preview__col relative w-full lg:w-1/2">
-            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="absolute w-full h-full top-0 left-0 object-cover">
-        </div>
-        <div class="post-preview__col w-full lg:w-1/2">
-            <div class="flex flex-wrap">
-                <div class="post-preview__desc w-full lg:w-8/12 pt-8">
-                    <div class="post-preview__cat text-center text-lg mb-8">BRANDING</div>
-                    <h3 class="text-center text-2xl"><?php the_title(); ?></h3>
-                    <?php the_excerpt(); ?>
+    <article>
+        <a href="<?php the_permalink(); ?>" class="flex flex-wrap">
+            <div class="post-preview__img post-preview__col relative w-full lg:w-1/2">
+                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="absolute w-full h-full top-0 left-0 object-cover">
+            </div>
+            <div class="post-preview__col w-full lg:w-1/2">
+                <div class="flex flex-wrap">
+                    <div class="post-preview__desc w-full lg:w-8/12 pt-8">
+                        <div class="post-preview__cat text-center text-lg mb-8">BRANDING</div>
+                        <h3 class="text-center text-2xl"><?php the_title(); ?></h3>
+                        <div class="text-left">
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </a>
+        </a>
+    </article>
 </div>
