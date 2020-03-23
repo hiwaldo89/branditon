2d53e388b91c3d022289170b8321681f618fd5ca
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 18:58:41 -0600

    tweek styles

--- home.php
+++ home.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
     <main>
         <?php if(have_posts()) : ?>
-            <div class="container mx-auto text-center">
-                <h2 class="text-3xl pt-20 mb-12"><?php the_field('headline', get_option('page_for_posts')); ?></h2>
+            <div class="container mx-auto text-center py-20">
+                <h2 class="text-2xl mt-12 mb-12 pb-20 font-secondary tracking-widest"><?php the_field('headline', get_option('page_for_posts')); ?></h2>
                 <?php while(have_posts()) : the_post(); ?>
                     <?php get_template_part('components/blog/post-preview'); ?>
                 <?php endwhile; wp_reset_postdata(); ?>
