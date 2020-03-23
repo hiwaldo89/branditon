a76cf7329915da4f9ce32b04d4befbb8c4f5b280
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 19:45:22 -0600

    tweek styles

--- components/contact/contact-section.php
+++ components/contact/contact-section.php
@@ -1,13 +1,13 @@
 <div class="container py-20 mx-auto text-center contact-section">
     <h2 class="text-2xl mt-12 font-secondary--italic tracking-widest">¿Cómo puedes comunicarte directamente conmigo?</h2>
     <p class="text-2xl mb-16 font-secondary--italic tracking-widest">Envíame un correo a:</p>
-    <a href="mailto:fernanda@branditon.com.mx" class="inline-block text-2xl mb-16 contact-section__email font-secondary tracking-widest">fernanda@branditon.com.mx</a>
-    <div class="flex items-center justify-center contact-section__social mb-8">
+    <a href="mailto:fernanda@branditon.com.mx" class="inline-block text-xl mb-16 contact-section__email font-secondary tracking-widest">fernanda@branditon.com.mx</a>
+    <div class="flex items-center justify-center contact-section__social mb-16">
         <a href="https://www.instagram.com/brand.it.on/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/instagram-circle.svg" alt="instagram"></a>
         <a href="https://www.facebook.com/branditon.mx/" target="_blank" class="mx-4"><img src="<?php bloginfo('template_url'); ?>/img/facebook-circle.svg" alt="facebook"></a>
         <a href="mailto:fernanda@branditon.com.mx"><img src="<?php bloginfo('template_url'); ?>/img/email-circle.svg" alt="email"></a>
     </div>
-    <p class="text-3xl">
+    <p class="text-2xl font-secondary--italic tracking-widest">
         o llena la siguiente forma y me pondré en 
         <br>
         contacto contigo:
