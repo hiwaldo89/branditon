44f219bba918bdb082294f9653652bcd38005e68
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-23 17:57:11 -0600

    wire up newsletter form

--- src/js/components/_footer.js
+++ src/js/components/_footer.js
@@ -18,6 +18,7 @@ mailchimpForm.onsubmit = e => {
     data: new FormData(mailchimpForm)
   })
     .then(res => {
+      console.log(res);
       if (res.data == 'error') {
         mailchimpFormMessage.querySelector('span').innerHTML =
           'Ocurrió un error. Intenta de nuevo.';
