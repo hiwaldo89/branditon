3485c75ee95505b6844eaf9fe62abf90b0859e85
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-23 07:37:50 -0600

    add responsive styles

--- components/homepage/about.php
+++ components/homepage/about.php
@@ -1,7 +1,7 @@
 <?php $aboutSection = get_field('about_section'); ?>
 <?php if ($aboutSection) : ?>
     <div class="homepage-about py-12">
-        <div class="container mx-auto flex">
+        <div class="container mx-auto flex flex-wrap">
             <div class="w-full md:w-4/12 lg:w-3/12">
                 <span class="homepage-about__line-deco"></span>
                 <h2 class="text-4xl text-center tracking-widest"><?php echo $aboutSection['headline']; ?></h2>
@@ -18,7 +18,7 @@
                         <img src="<?php echo $aboutSection['front_image']['url']; ?>" alt="<?php echo $aboutSection['front_image']['alt']; ?>" class="w-full">
                     </div>
                 </div>
-                <div class="w-full md:w-7/12 homepage-about__back-img md:ml-auto">
+                <div class="w-full md:w-7/12 homepage-about__back-img md:ml-auto hidden md:block">
                     <img src="<?php echo $aboutSection['back_image']['url']; ?>" alt="<?php echo $aboutSection['back_image']['alt']; ?>" class="w-full">
                 </div>
             </div>
