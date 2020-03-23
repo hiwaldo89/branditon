0afcfd89a0cebb099704fcbecc676867d666545f
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 19:30:13 -0600

    tweek styles

--- components/blog/post-nav.php
+++ components/blog/post-nav.php
@@ -4,15 +4,15 @@
         <?php $prevPost = get_posts()[0]; ?>
     <?php endif; ?>
     <a href="<?php echo get_permalink($prevPost->ID); ?>" class="w-full lg:w-1/2 post-nav__prev text-right pt-24 px-20 pb-20">
-        <span class="text-lg">ANTERIOR</span>
-        <h3 class="text-2xl"><?php echo $prevPost->post_title; ?></h3>
+        <span class="text-sm tracking-widest">ANTERIOR</span>
+        <h3 class="text-2xl font-secondary tracking-widest"><?php echo $prevPost->post_title; ?></h3>
     </a>
     <?php $nextPost = get_next_post(); ?>
     <?php if(!isset($nextPost) || strlen($nextPost->post_title) <= 0) : ?>
         <?php $nextPost = get_posts( 'numberposts=1&order=ASC' )[0]; ?>
     <?php endif; ?>
     <a href="<?php echo get_permalink($nextPost->ID); ?>" class="w-full lg:w-1/2 post-nav__next pt-24 px-20">
-        <span class="text-lg">SIGUIENTE</span>
-        <h3 class="text-2xl"><?php echo $nextPost->post_title; ?></h3>
+        <span class="text-sm tracking-widest">SIGUIENTE</span>
+        <h3 class="text-2xl font-secondary tracking-widest"><?php echo $nextPost->post_title; ?></h3>
     </a>
 </div>
--- components/blog/share-article.php
+++ components/blog/share-article.php
@@ -1,6 +1,6 @@
 <div class="share-article py-24">
     <div class="container mx-auto text-center">
-        <a href="" class="uppercase flex items-center justify-center">
+        <a href="" class="uppercase flex items-center justify-center tracking-widest">
             COMPARTE ESTE ARTÍCULO
             <span class="share-article__plus ml-8"></span>
         </a>
