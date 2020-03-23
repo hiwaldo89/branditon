378689fea4d0d89cfcd2e33fc48f2b5ee7f4c08a
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 19:30:42 -0600

    tweek styles

--- components/blog/post-nav.php
+++ components/blog/post-nav.php
@@ -5,7 +5,7 @@
     <?php endif; ?>
     <a href="<?php echo get_permalink($prevPost->ID); ?>" class="w-full lg:w-1/2 post-nav__prev text-right pt-24 px-20 pb-20">
         <span class="text-sm tracking-widest">ANTERIOR</span>
-        <h3 class="text-2xl font-secondary tracking-widest"><?php echo $prevPost->post_title; ?></h3>
+        <h3 class="text-xl font-secondary tracking-widest"><?php echo $prevPost->post_title; ?></h3>
     </a>
     <?php $nextPost = get_next_post(); ?>
     <?php if(!isset($nextPost) || strlen($nextPost->post_title) <= 0) : ?>
@@ -13,6 +13,6 @@
     <?php endif; ?>
     <a href="<?php echo get_permalink($nextPost->ID); ?>" class="w-full lg:w-1/2 post-nav__next pt-24 px-20">
         <span class="text-sm tracking-widest">SIGUIENTE</span>
-        <h3 class="text-2xl font-secondary tracking-widest"><?php echo $nextPost->post_title; ?></h3>
+        <h3 class="text-xl font-secondary tracking-widest"><?php echo $nextPost->post_title; ?></h3>
     </a>
 </div>
