4743c25a5368775a670b2665469332da0aac8004
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 18:43:50 -0600

    tweek styles

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,7 @@
 <footer class="branditon-footer">
     <div class="flex flex-wrap">
         <div class="w-full md:w-1/2 branditon-footer__social text-center py-32 flex flex-col items-center">
-            <h2 class="text-3xl mb-6">
+            <h2 class="text-2xl mb-6 font-italic">
                 Creating a brand isn't 
                 <br>
                 about creating a logo
