464bd65fd065d2da0284644403f9abce90eccdd6
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 18:13:07 -0600

    adding radio buttons to contact form

--- components/contact/contact-form.php
+++ components/contact/contact-form.php
@@ -3,6 +3,16 @@
         <div class="flex flex-wrap">
             <div class="w-full lg:w-8/12 mx-auto">
                 <form action="">
+                    <div class="d-flex flex-wrap">
+                        <label class="container">¡DI HOLA!
+                            <input type="radio" name="subject" checked="checked">
+                            <span class="checkmark"></span>
+                        </label>
+                        <label class="container">¿DUDAS?
+                            <input type="radio" name="subject">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
                     <input type="text" placeholder="Nombre">
                     <input type="tel" placeholder="Teléfono">
                     <input type="email" placeholder="Correo">
