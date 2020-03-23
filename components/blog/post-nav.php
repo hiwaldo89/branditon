60692560b591dcc98c95b69667214e649a07a4de
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-23 09:16:58 -0600

    add responsive styles

--- components/blog/post-nav.php
+++ components/blog/post-nav.php
@@ -11,7 +11,7 @@
     <?php if(!isset($nextPost) || strlen($nextPost->post_title) <= 0) : ?>
         <?php $nextPost = get_posts( 'numberposts=1&order=ASC' )[0]; ?>
     <?php endif; ?>
-    <a href="<?php echo get_permalink($nextPost->ID); ?>" class="w-full lg:w-1/2 post-nav__next pt-24 px-20">
+    <a href="<?php echo get_permalink($nextPost->ID); ?>" class="w-full lg:w-1/2 post-nav__next pt-24 px-20 pb-20">
         <span class="text-sm tracking-widest">SIGUIENTE</span>
         <h3 class="text-xl font-secondary tracking-widest"><?php echo $nextPost->post_title; ?></h3>
     </a>
