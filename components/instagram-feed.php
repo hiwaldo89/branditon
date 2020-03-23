8a925957df8eefae25bd3be9175903d7b5737e24
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-22 18:42:02 -0600

    tweek styles

--- components/instagram-feed.php
+++ components/instagram-feed.php
@@ -1,7 +1,7 @@
 <div class="instagram-feed py-12">
     <div class="container mx-auto text-center">
-        <h2 class="text-2xl">INSTAGRAM</h2>
-        <a href="" class="text-2xl">@brand.it.on</a>
+        <h2 class="text-xl tracking-widest">INSTAGRAM</h2>
+        <a href="" class="text-xl tracking-widest">@brand.it.on</a>
     </div>
     <div class="instagram-feed__slider">
         Here goes the instagram feed
