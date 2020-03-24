deed160ddcf9075391c126a273df23a57b7fe0b1
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 11:42:53 -0600

    add parallax

--- components/blog/post-nav.php
+++ components/blog/post-nav.php
@@ -1,6 +1,6 @@
 <div class="post-nav flex flex-wrap">
     <?php $prevPost = get_previous_post(); ?>
-    <?php if(!isset($prevPost) || strlen($prevPost->post_title) <= 0) : ?>
+    <?php if($prevPost != '') : ?>
         <?php $prevPost = get_posts()[0]; ?>
     <?php endif; ?>
     <a href="<?php echo get_permalink($prevPost->ID); ?>" class="w-full lg:w-1/2 post-nav__prev lg:text-right pt-24 px-20 pb-20">
@@ -8,7 +8,7 @@
         <h3 class="text-xl font-secondary tracking-widest"><?php echo $prevPost->post_title; ?></h3>
     </a>
     <?php $nextPost = get_next_post(); ?>
-    <?php if(!isset($nextPost) || strlen($nextPost->post_title) <= 0) : ?>
+    <?php if($nextPost == '') : ?>
         <?php $nextPost = get_posts( 'numberposts=1&order=ASC' )[0]; ?>
     <?php endif; ?>
     <a href="<?php echo get_permalink($nextPost->ID); ?>" class="w-full lg:w-1/2 post-nav__next pt-24 px-20 pb-20">
