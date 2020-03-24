9ba39d4019ba45f6202b4260d4bef394ee25dfef
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 10:41:13 -0600

    add parallax

--- components/homepage/about.php
+++ components/homepage/about.php
@@ -2,7 +2,7 @@
 <?php if ($aboutSection) : ?>
     <div class="homepage-about py-12">
         <div class="container mx-auto flex flex-wrap px-4 lg:px-0 items-center">
-            <div class="w-full md:w-5/12 lg:w-3/12" data-aos="fade-left">
+            <div class="w-full md:w-5/12 lg:w-3/12" data-aos="fade-right">
                 <span class="homepage-about__line-deco"></span>
                 <h2 class="text-4xl text-center tracking-widest"><?php echo $aboutSection['headline']; ?></h2>
                 <div class="text-justify text-xl">
@@ -13,12 +13,12 @@
                 </a>
             </div>
             <div class="w-full md:w-7/12 md:ml-auto homepage-about__images relative">
-                <div class="w-full md:w-7/12 homepage-about__front-img z-10" data-rellax-speed="2" data-rellax-mobile-speed="0" data-rellax-xs-speed="0">
+                <div class="w-full md:w-7/12 homepage-about__front-img z-10" data-rellax-speed="2" data-rellax-mobile-speed="0" data-rellax-xs-speed="0" data-aos="fade-up">
                     <div>
                         <img src="<?php echo $aboutSection['front_image']['url']; ?>" alt="<?php echo $aboutSection['front_image']['alt']; ?>" class="w-full">
                     </div>
                 </div>
-                <div class="w-full md:w-7/12 homepage-about__back-img md:ml-auto" data-rellax-speed="3" data-rellax-mobile-speed="0" data-rellax-xs-speed="0">
+                <div class="w-full md:w-7/12 homepage-about__back-img md:ml-auto" data-rellax-speed="3" data-rellax-mobile-speed="0" data-rellax-xs-speed="0" data-aos="fade-up">
                     <img src="<?php echo $aboutSection['back_image']['url']; ?>" alt="<?php echo $aboutSection['back_image']['alt']; ?>" class="w-full">
                 </div>
             </div>
