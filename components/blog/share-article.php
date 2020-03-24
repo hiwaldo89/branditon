b0f12d58870e54b9e9f4bb28f76e65e4d7e0a48b
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 11:58:26 -0600

    add hover state to header logo and social icons

--- components/blog/share-article.php
+++ components/blog/share-article.php
@@ -1,6 +1,7 @@
 <div class="share-article py-24">
     <div class="container mx-auto text-center">
-        <a href="" class="uppercase flex items-center justify-center tracking-widest">
+        <?php $obj_id = get_queried_object_id(); ?>
+        <a href="https://www.facebook.com/dialog/share?app_id=852345838524571&display=popup&href=<?php echo get_permalink( $obj_id ); ?>" class="uppercase flex items-center justify-center tracking-widest">
             COMPARTE ESTE ARTÍCULO
             <span class="share-article__plus ml-8"></span>
         </a>
