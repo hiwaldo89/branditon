674f51c42bbb18acad7cfdae03c75e637869ca81
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-23 19:02:45 -0600

    wiring instagram feed

--- components/instagram-feed.php
+++ components/instagram-feed.php
@@ -18,7 +18,7 @@
         <?php foreach($instagramFeed as $image) : ?>
             <div class="instagram-feed__slide">
                 <a href="<?php echo $image['permalink']; ?>" target="_blank">
-                    <img src="<?php echo $image['media_url']; ?>" alt="<?php echo $image['username']; ?>">
+                    <img src="<?php echo $image['media_url']; ?>" alt="<?php echo $image->username; ?>">
                 </a>
             </div>
         <?php endforeach; ?>
