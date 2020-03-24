16125d7f28a78789d3491c74da91e40016fc808c
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 09:44:25 -0600

    add parallax

--- src/js/components/homepage/_about.js
+++ src/js/components/homepage/_about.js
@@ -1,2 +1,7 @@
 'use strict';
 import simpleParallax from 'simple-parallax-js';
+
+const frontImage = document.querySelector('.homepage-about__front-img');
+new simpleParallax(frontImage, {
+  orientation: 'up'
+});
