5290fa3c012c12444ce35c32ef96de88ca342669
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 09:48:52 -0600

    add parallax

--- src/js/components/homepage/_about.js
+++ src/js/components/homepage/_about.js
@@ -3,5 +3,7 @@ import simpleParallax from 'simple-parallax-js';
 
 const frontImage = document.querySelector('.homepage-about__front-img');
 new simpleParallax(frontImage, {
-  orientation: 'up'
+  orientation: 'up',
+  overflow: true,
+  scale: 1.2
 });
