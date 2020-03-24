694070a61f4bfa30f5c4ef88747e45d9a9979e29
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-23 19:23:10 -0600

    wiring instagram feed

--- src/js/components/homepage/_instagram-feed.js
+++ src/js/components/homepage/_instagram-feed.js
@@ -5,6 +5,14 @@ if (document.querySelector('.instagram-feed')) {
   $('.instagram-feed__slider').slick({
     slidesToShow: 5,
     centerMode: true,
-    arrows: false
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
   });
 }
