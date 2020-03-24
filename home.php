126f51d386f2a7c9bd199e4d44cab2bc6dd479dc
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 08:36:19 -0600

    tweak mobile styes

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <main>
         <?php if(have_posts()) : ?>
-            <div class="container mx-auto text-center pt-20 pb-8 px-4 md:px-0">
+            <div class="container mx-auto text-center pt-20 pb-8 px-4 lg:px-0">
                 <h2 class="text-2xl mt-12 mb-12 pb-20 font-secondary tracking-widest"><?php the_field('headline', get_option('page_for_posts')); ?></h2>
                 <?php while(have_posts()) : the_post(); ?>
                     <?php get_template_part('components/blog/post-preview'); ?>
