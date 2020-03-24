fb52db4108848441c5f9fe612cfe83dc6c972866
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 09:56:52 -0600

    add parallax

--- components/homepage/about.php
+++ components/homepage/about.php
@@ -13,7 +13,7 @@
                 </a>
             </div>
             <div class="w-full md:w-7/12 md:ml-auto homepage-about__images relative">
-                <div class="w-full md:w-7/12 homepage-about__front-img">
+                <div class="w-full md:w-7/12 homepage-about__front-img" data-rellax-speed="5">
                     <div>
                         <img src="<?php echo $aboutSection['front_image']['url']; ?>" alt="<?php echo $aboutSection['front_image']['alt']; ?>" class="w-full">
                     </div>
