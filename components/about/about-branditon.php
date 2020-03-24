bc0af65e7b18ffa89ef036bf8c6b3f678df1cc14
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 08:33:06 -0600

    tweak mobile styes

--- components/about/about-branditon.php
+++ components/about/about-branditon.php
@@ -1,4 +1,4 @@
-<div class="container mx-auto about-branditon py-20 px-4 md:px-0">
+<div class="container mx-auto about-branditon py-20 px-4 lg:px-0">
     <h2 class="text-center text-2xl mt-12 mb-12 pb-20 font-secondary tracking-widest">Acerca de Brand it on</h2>
     <div class="flex flex-wrap about-branditon__content mb-24">
         <div class="w-full lg:w-1/2 text-justify about-branditon__col text-xl mb-8 lg:mb-0">
@@ -11,7 +11,7 @@
     <div class="flex flex-wrap about-branditon__content">
         <?php if(have_rows('images')) : while(have_rows('images')) : the_row(); ?>
             <?php $image = get_sub_field('image'); ?>
-            <div class="w-full lg:w-1/2 about-branditon__col mb-6 lg:mb-0">
+            <div class="w-full md:w-1/2 about-branditon__col mb-6 lg:mb-0">
                 <div class="about-branditon__img relative">
                     <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="absolute w-full h-full top-0 left-0 object-cover">
                 </div>
