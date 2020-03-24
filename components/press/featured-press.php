4c864b00ec79c10037f0e791d9e10caf20693fdd
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 08:45:44 -0600

    tweak mobile styes

--- components/press/featured-press.php
+++ components/press/featured-press.php
@@ -1,10 +1,10 @@
 <?php $f = new NumberFormatter("es", NumberFormatter::SPELLOUT); ?>
 <div class="featured-press">
     <a href="<?php the_field('article_link'); ?>" target="_blank" class="flex flex-wrap">
-        <div class="featured-press__img w-full lg:w-8/12 relative">
+        <div class="featured-press__img w-full md:w-8/12 relative">
             <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="absolute w-full h-full object-cover top-0 left-0">
         </div>
-        <div class="w-full lg:w-4/12 featured-press__content text-center flex flex-col justify-center px-12 py-8">
+        <div class="w-full md:w-4/12 featured-press__content text-center flex flex-col justify-center px-12 py-8">
             <?php 
                 $mediaPublication = get_the_terms(get_the_ID(), 'publication_media'); 
             ?>
