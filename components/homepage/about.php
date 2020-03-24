330ff7437ab9000e7323989dbd13e1518875e661
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 10:49:34 -0600

    add parallax

--- components/homepage/about.php
+++ components/homepage/about.php
@@ -18,7 +18,7 @@
                         <img src="<?php echo $aboutSection['front_image']['url']; ?>" alt="<?php echo $aboutSection['front_image']['alt']; ?>" class="w-full">
                     </div>
                 </div>
-                <div class="w-full md:w-7/12 homepage-about__back-img md:ml-auto" data-rellax-speed="-2" data-rellax-mobile-speed="0" data-rellax-xs-speed="0">
+                <div class="w-full md:w-7/12 homepage-about__back-img md:ml-auto" data-rellax-speed="2" data-rellax-mobile-speed="0" data-rellax-xs-speed="0">
                     <img src="<?php echo $aboutSection['back_image']['url']; ?>" alt="<?php echo $aboutSection['back_image']['alt']; ?>" class="w-full">
                 </div>
             </div>
