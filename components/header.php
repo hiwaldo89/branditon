395031c4f58fb85cdc8e511b192298a196244018
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-03-24 08:30:22 -0600

    tweak mobile styes

--- components/header.php
+++ components/header.php
@@ -1,16 +1,16 @@
 <header class="branditon-header py-4">
-    <div class="container mx-auto flex items-center px-4 md:px-0">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="mr-auto branditon-header__logo w-1/2 md:w-3/12">
+    <div class="container mx-auto flex items-center px-4 lg:px-0">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="mr-auto branditon-header__logo w-1/2 lg:w-3/12">
             <img src="<?php bloginfo('template_url'); ?>/img/branditon-logo.svg" alt="branditon">
         </a>
-        <div class="md:hidden w-1/2 text-right branditon-header__hamburger">
+        <div class="lg:hidden w-1/2 text-right branditon-header__hamburger">
             <button class="hamburger hamburger--squeeze" type="button">
                 <span class="hamburger-box">
                     <span class="hamburger-inner"></span>
                 </span>
             </button>
         </div>
-        <nav id="main-navigation" class="mr-3 w-full md:w-1/2 flex items-center">
+        <nav id="main-navigation" class="mr-3 w-full lg:w-1/2 flex items-center">
             <?php global $post; ?>
             <?php $pageSlug = $post->post_name; ?>
             <ul class="flex flex-col md:flex-row w-full justify-between">
@@ -40,7 +40,7 @@
                 </li>
             </ul>
         </nav>
-        <div class="branditon-header__social hidden md:flex md:w-3/12 justify-end items-center">
+        <div class="branditon-header__social hidden lg:flex lg:w-3/12 justify-end items-center">
             <a href="https://www.instagram.com/brand.it.on/" target="_blank" class="mr-4"><img src="<?php bloginfo('template_url'); ?>/img/instagram.svg" alt="brand.it.on"></a>
             <a href="https://www.facebook.com/branditon.mx/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/facebook.svg" alt="branditon.mx"></a>
         </div>
